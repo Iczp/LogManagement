9c8084845164a7f7d80062a2834328bbdfe869d0
zhongpei <1000@intry.cn>
2024-07-17 09:43:20 +0800

    add-mig Abp82

--- host/IczpNet.LogManagement.AuthServer/Migrations/20240717014234_Abp8.2.Designer.cs
+++ host/IczpNet.LogManagement.AuthServer/Migrations/20240717014234_Abp8.2.Designer.cs
@@ -4,6 +4,7 @@ using IczpNet.LogManagement.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace IczpNet.LogManagement.Migrations
 {
     [DbContext(typeof(AuthServerDbContext))]
-    partial class AuthServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240717014234_Abp8.2")]
+    partial class Abp82
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
